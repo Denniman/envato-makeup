4fd174e6b0a21642bcf11945c49c253e521dc66e
Dennis <dennimanadams@gmail.com>
2020-07-25 10:51:04 +0100

    implement testimonial slider and change footer media icon

---
+++ js/custom.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    $('.testimonial-section').slick({
+        dots: true,
+        infinite: true,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        speed: 500,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        adaptiveHeight: true
+    });
+  });
